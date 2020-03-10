1c39f8723a7bcb5a5943022fde3a170d5767158c
Joe Pea <joe@trusktr.io>
2020-03-10 00:08:55 -0700

    format code with lume prettier

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
-import { observe, unobserve } from './observe'
+import {observe, unobserve} from './observe'
 
-export { observe, unobserve }
+export {observe, unobserve}
 
-type Handler = (arg: { paths: string[] }, ...args: any[]) => unknown
+type Handler = (arg: {paths: string[]}, ...args: any[]) => unknown
 
-export
-function deeplyObserve(object: object, handler: Handler, argumentArray: any[] = [], path = "") {
-    if (typeof object !== 'object' || !object) {
-        throw new Error("Cannot observe a non-object.");
-    }
+export function deeplyObserve(object: object, handler: Handler, argumentArray: any[] = [], path = '') {
+	if (typeof object !== 'object' || !object) {
+		throw new Error('Cannot observe a non-object.')
+	}
 
-    // requires Object.observe
-    ;(Object as any).observe(object, function(changes: any) {
-        var paths: any[] = [];
-        changes.forEach(function(change: any) {
-            paths.push(path.length? path+'.'+change.name: change.name);
-        });
-        handler({ paths }, ...argumentArray);
-    });
+	// requires Object.observe
+	;(Object as any).observe(object, function(changes: any) {
+		var paths: any[] = []
 
-    for (var prop in object) {
-        const value: object = (object as any)[prop]
-        if (value && typeof value === 'object') {
-            deeplyObserve(value, handler, argumentArray, (path.length? path+'.'+prop: prop) );
-        }
-    }
+		changes.forEach(function(change: any) {
+			paths.push(path.length ? path + '.' + change.name : change.name)
+		})
+
+		handler({paths}, ...argumentArray)
+	})
+
+	for (var prop in object) {
+		const value: object = (object as any)[prop]
+
+		if (value && typeof value === 'object') {
+			deeplyObserve(value, handler, argumentArray, path.length ? path + '.' + prop : prop)
+		}
+	}
 }
 
 export const version = '0.4.2'
--- src/observe.ts
+++ src/observe.ts
@@ -1,70 +1,68 @@
-import { getInheritedDescriptor } from 'lowclass'
+import {getInheritedDescriptor} from 'lowclass'
 
 type Options = {
-    async: boolean
-    inherited: boolean
+	async: boolean
+	inherited: boolean
 }
 
 type Callback = (propName: string, value: any) => unknown
 
 const propsAndCallbacks = new WeakMap<object, Map<string, Callback[]>>()
 
-export
-function observe(object: object, propertyNames: string[], callback: Callback, options: Partial<Options> = {}) {
-    // TODO the options.async option will make callbacks fire on the next microtask instead of synchronously
-    options.async = options.async || false
-    options.inherited = options.inherited || false
+export function observe(object: object, propertyNames: string[], callback: Callback, options: Partial<Options> = {}) {
+	// TODO the options.async option will make callbacks fire on the next microtask instead of synchronously
+	options.async = options.async || false
+	options.inherited = options.inherited || false
 
-    for (const propName of propertyNames) {
+	for (const propName of propertyNames) {
+		let propCallbacks = propsAndCallbacks.get(object)
+		!propCallbacks && propsAndCallbacks.set(object, (propCallbacks = new Map()))
 
-        let propCallbacks = propsAndCallbacks.get(object)
-        !propCallbacks && propsAndCallbacks.set(object, propCallbacks = new Map)
+		let callbacks = propCallbacks.get(propName)
 
-        let callbacks = propCallbacks.get(propName)
+		if (callbacks) {
+			if (!callbacks.includes(callback)) callbacks.push(callback)
+			continue
+		}
 
-        if (callbacks) {
-            if (!callbacks.includes(callback)) callbacks.push(callback)
-            continue
-        }
+		// the rest only runs once, the first time the prop observation is set up
 
-        // the rest only runs once, the first time the prop observation is set up
+		propCallbacks.set(propName, (callbacks = []))
+		callbacks.push(callback)
 
-        propCallbacks.set(propName, callbacks = [])
-        callbacks.push(callback)
-
-        defineObservationGetterSetter(object, propName, options as Options)
-    }
+		defineObservationGetterSetter(object, propName, options as Options)
+	}
 }
 
 // NOTE, unobserve does not remove the observation accessors that observe
 // creates. It might be nice if it did, so that objects can return to their lean
 // shape. TODO can we do it?
-export
-function unobserve(object: object, props: Callback | string[], callback?: Callback) {
-    const propCallbacks = propsAndCallbacks.get(object)
+export function unobserve(object: object, props: Callback | string[], callback?: Callback) {
+	const propCallbacks = propsAndCallbacks.get(object)
 
-    if (!propCallbacks) {
-        console.warn('the object is not observed, no need to unobserve:', object)
-        return
-    }
+	if (!propCallbacks) {
+		console.warn('the object is not observed, no need to unobserve:', object)
+		return
+	}
 
-    // If called as unobserve(object, callback), unobserve all props for the callback.
-    if (typeof props === 'function') {
-        callback = props
-        props = Array.from( propCallbacks.keys() )
-    }
+	// If called as unobserve(object, callback), unobserve all props for the callback.
+	if (typeof props === 'function') {
+		callback = props
+		props = Array.from(propCallbacks.keys())
+	}
 
-    // Otherwise called as unobserve(object, props, callback), so unobserve the specific props for the callback.
+	// Otherwise called as unobserve(object, props, callback), so unobserve the specific props for the callback.
 
-    if (!callback) throw new TypeError('callback not supplied')
+	if (!callback) throw new TypeError('callback not supplied')
 
-    for (const prop of props) {
-        const callbacks = propCallbacks.get(prop)!
-        if (callbacks.includes(callback)) {
-            callbacks.splice(callbacks.indexOf(callback), 1)
-            if (!callbacks.length) propCallbacks.delete(prop)
-        }
-    }
+	for (const prop of props) {
+		const callbacks = propCallbacks.get(prop)!
+
+		if (callbacks.includes(callback)) {
+			callbacks.splice(callbacks.indexOf(callback), 1)
+			if (!callbacks.length) propCallbacks.delete(prop)
+		}
+	}
 }
 
 // This is used to keep track if an object already has an observation accessor
@@ -73,84 +71,91 @@ function unobserve(object: object, props: Callback | string[], callback?: Callba
 const objectsToObservableProps = new WeakMap<object, Set<string>>()
 
 function defineObservationGetterSetter(object: object, propName: string, options: Options) {
-    let observableProps: Set<string> | undefined
-    const inherited = options.inherited
+	let observableProps: Set<string> | undefined
+	const inherited = options.inherited
 
-    if (!inherited) {
-        observableProps = objectsToObservableProps.get(object)
-        if (!observableProps) objectsToObservableProps.set(object, observableProps = new Set)
-        else if (observableProps.has(propName)) return
-    }
+	if (!inherited) {
+		observableProps = objectsToObservableProps.get(object)
+		if (!observableProps) objectsToObservableProps.set(object, (observableProps = new Set()))
+		else if (observableProps.has(propName)) return
+	}
 
-    // get the existing descriptor, or create a new one if the property doesn't exist.
-    const descriptor = getInheritedDescriptor(object, propName) || ({
-        value: undefined,
-        writable: true,
-        configurable: true,
-        enumerable: true,
-    } as ReturnType<typeof getInheritedDescriptor>)!
+	// get the existing descriptor, or create a new one if the property doesn't exist.
+	const descriptor =
+		getInheritedDescriptor(object, propName) ||
+		({
+			value: undefined,
+			writable: true,
+			configurable: true,
+			enumerable: true,
+		} as ReturnType<typeof getInheritedDescriptor>)!
 
-    const owner = options.inherited ? descriptor.owner || object : object
+	const owner = options.inherited ? descriptor.owner || object : object
 
-    if (inherited) {
-        // TODO, this check probably actually needs to look up on the prototype
-        // chain, because people can modify prototype chains and introduce new
-        // descriptors anywhere in the chain. We want to check the whole chain
-        // to see if we find an observation accessor defined by us.
-        observableProps = objectsToObservableProps.get(owner)
-        if (!observableProps) objectsToObservableProps.set(owner, observableProps = new Set)
-        else if (observableProps.has(propName)) return
-    }
+	if (inherited) {
+		// TODO, this check probably actually needs to look up on the prototype
+		// chain, because people can modify prototype chains and introduce new
+		// descriptors anywhere in the chain. We want to check the whole chain
+		// to see if we find an observation accessor defined by us.
+		observableProps = objectsToObservableProps.get(owner)
+		if (!observableProps) objectsToObservableProps.set(owner, (observableProps = new Set()))
+		else if (observableProps.has(propName)) return
+	}
 
-    let getValue: (() => any) | undefined
-    let setValue: ((v: any) => void) | undefined
+	let getValue: (() => any) | undefined
+	let setValue: ((v: any) => void) | undefined
 
-    if (descriptor.get || descriptor.set) {
-        // we will use the existing getter/setter assuming they don't do
-        // anyting crazy that we might not expect. (See? Another reason for
-        // Object.observe)
-        const oldGet = descriptor.get
-        const oldSet = descriptor.set
+	if (descriptor.get || descriptor.set) {
+		// we will use the existing getter/setter assuming they don't do
+		// anyting crazy that we might not expect. (See? Another reason for
+		// Object.observe)
+		const oldGet = descriptor.get
+		const oldSet = descriptor.set
 
-        getValue = oldGet ? () => oldGet.call(object) : undefined
-        setValue = oldSet ? value => oldSet.call(object, value) : undefined
-    }
-    else {
-        let _value = descriptor.value
+		getValue = oldGet ? () => oldGet.call(object) : undefined
+		setValue = oldSet ? value => oldSet.call(object, value) : undefined
+	} else {
+		let _value = descriptor.value
 
-        if (!descriptor.writable) {
-            console.error(`Can not observe readonly property "${propName}" with descriptor`, descriptor, `of object:`, object)
-            throw new Error(`Can not observe readonly property "${propName}" of object: ${object} (see above)`)
-        }
+		if (!descriptor.writable) {
+			console.error(
+				`Can not observe readonly property "${propName}" with descriptor`,
+				descriptor,
+				`of object:`,
+				object,
+			)
 
-        delete descriptor.value
-        delete descriptor.writable
+			throw new Error(`Can not observe readonly property "${propName}" of object: ${object} (see above)`)
+		}
 
-        getValue = () => _value
-        setValue = value => _value = value
-    }
+		delete descriptor.value
+		delete descriptor.writable
 
-    if (!setValue) throw new Error('Can not observe read-only property.')
-    if (!getValue) throw new Error('Can not observe write-only property.')
+		getValue = () => _value
+		setValue = value => (_value = value)
+	}
 
-    Object.defineProperty(owner, propName, {
-        ...descriptor,
-        get: getValue,
-        set(value) {
-            setValue!(value)
-            runCallbacks(object, propName, getValue!())
-        },
-    })
+	if (!setValue) throw new Error('Can not observe read-only property.')
+	if (!getValue) throw new Error('Can not observe write-only property.')
 
-    observableProps!.add(propName)
+	Object.defineProperty(owner, propName, {
+		...descriptor,
+		get: getValue,
+		set(value) {
+			setValue!(value)
+			runCallbacks(object, propName, getValue!())
+		},
+	})
+
+	observableProps!.add(propName)
 }
 
 function runCallbacks(object: object, propName: string, value: any) {
-    const callbacks = propsAndCallbacks.get(object)!.get(propName)
+	const callbacks = propsAndCallbacks.get(object)!.get(propName)
 
-    if (!callbacks) return
+	if (!callbacks) return
 
-    for (const callback of callbacks) {
-        callback(propName, value)
-    }
+	for (const callback of callbacks) {
+		callback(propName, value)
+	}
 }
